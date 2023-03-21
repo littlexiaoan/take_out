6ab1a31b5be3186c1267ff267a2bf5c4619bbeb5
anguobin <2430277611@qq.com>
2023-03-21 10:04:22 +0800

    第二次提交

---
+++ src/main/java/com/xiaoan/reggie/common/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.xiaoan.reggie.common;
+
+/**
+ * @author : little_ann
+ * @Date : 2023/03/20/16:26
+ */
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+/**
+ * 全局异常处理
+ */
+@ControllerAdvice(annotations = {RestController.class, Controller.class}) //拦截
+@ResponseBody //返回json数据
+@Slf4j
+public class GlobalExceptionHandler {
+
+    /**
+     * 异常处理方法
+     *
+     * @return
+     */
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public R<String> exceptionHandler(SQLIntegrityConstraintViolationException ex) {
+        log.error(ex.getMessage());
+
+        //用户名重复
+        if (ex.getMessage().contains("Duplicate entry")) {
+            String[] split = ex.getMessage().split(" ");
+            String msg = split[2] + "已存在";
+            return R.error(msg);
+        }
+        return R.error("未知错误");
+    }
+}
---
+++ src/main/java/com/xiaoan/reggie/config/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+package com.xiaoan.reggie.config;
+
+/**
+ * @author : little_ann
+ * @Date : 2023/03/20/19:19
+ */
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置MP的分页插件
+ */
+
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
